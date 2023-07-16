f79212aa93ce95c301d44dea94c5c29f86b7c620
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-16 11:13:36 +0530

    260. Strapi - Components

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import { FaBars } from 'react-icons/fa';
+import useGlobalContext from './Context';
+
+const Navbar = () => {
+  const { openSidebar } = useGlobalContext();
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3 className="logo">strapi</h3>
+        <button className="toggle-btn">
+          <FaBars />
+        </button>
+        {/* Nav links Later */}
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
