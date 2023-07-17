47c53b06284641a0c8c6a6b740c293928a6d345b
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-17 08:34:50 +0530

    263. Strapi - Sidebar JSX

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,36 @@
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './Context';
+import sublinks from './data';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn">{<FaTimes />}</button>
+      </div>
+      <div className="sidebarLinks">
+        {sublinks.map((item) => {
+          const { pageId, page, links } = item;
+          return (
+            <article key={pageId}>
+              <h4>{page}</h4>
+              <div className="sidebar-sublinks">
+                {links.map((link) => {
+                  const { url, icon, label, id } = link;
+                  return (
+                    <a key={id} href={url}>
+                      {icon}
+                      {label}
+                    </a>
+                  );
+                })}
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </aside>
+  );
 };
 export default Sidebar;
