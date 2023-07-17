b85d172897fad2309edb31d46210ae45a22fba67
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-17 20:05:31 +0530

    267. Strapi - Submenu JSX

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,4 +1,31 @@
+import sublinks from './data';
+import { useGlobalContext } from './Context';
+
 const Submenu = () => {
-  return <div>Submenu</div>;
+  const { pageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+  // console.log(currentPage);
+  return (
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, label, icon, url } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {url}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
