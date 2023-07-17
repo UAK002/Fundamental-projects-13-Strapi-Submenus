88b49c2ae5c8bc83cbaa0977e96bb399f8c1706b
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-07-17 09:55:11 +0530

    264. Strapi - Sidebar CSS

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,30 +5,32 @@ import sublinks from './data';
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext();
   return (
-    <aside className="sidebar">
+    <aside className={isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}>
       <div className="sidebar-container">
-        <button className="close-btn">{<FaTimes />}</button>
-      </div>
-      <div className="sidebarLinks">
-        {sublinks.map((item) => {
-          const { pageId, page, links } = item;
-          return (
-            <article key={pageId}>
-              <h4>{page}</h4>
-              <div className="sidebar-sublinks">
-                {links.map((link) => {
-                  const { url, icon, label, id } = link;
-                  return (
-                    <a key={id} href={url}>
-                      {icon}
-                      {label}
-                    </a>
-                  );
-                })}
-              </div>
-            </article>
-          );
-        })}
+        <button className="close-btn" onClick={closeSidebar}>
+          {<FaTimes />}
+        </button>
+        <div className="sidebarLinks">
+          {sublinks.map((item) => {
+            const { pageId, page, links } = item;
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link;
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
       </div>
     </aside>
   );
